dd8698de974459059ee176f2434a6977fe39a343
Ignas Dailydė <ignas.dailyde@gmail.com>
2019-02-11 12:23:47 +0200

    v0.1

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+    string vardas;
+
+    cout << "Iveskite varda: " << endl;
+
+    cin >> vardas;
+
+    cout << endl;
+
+    string pasisveikinimas = "Sveikas, " + vardas + "!";
+
+    int lineLength = pasisveikinimas.length() + 4;
+
+    string eil1;
+    string eil2;
+    string eil3;
+    string eil4;
+    string eil5;
+
+    for (int i = 0; i < lineLength; i++) {
+        eil1 += "*";
+    }
+
+    eil2 += "*";
+    for (int i = 0; i < lineLength-2; i++) {
+        eil2 += " ";
+    }
+    eil2 += "*";
+
+    eil3 = "* " + pasisveikinimas + " *";
+
+
+    eil4 = eil2;
+    eil5 = eil1;
+
+    cout << eil1 << endl << eil2 << endl << eil3 << endl << eil4 << endl << eil5;
+
+    return 0;
+}
